5608699cd916f542a8f62b6a7a5ddbb655bfc7f7
Andreas Finger <webmaster@mediafinger.com>
2012-03-07 22:32:40 +0100

    Fix auth bug

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,34 +3,34 @@ class AuthenticationsController < ApplicationController
     @authentications = current_user.authentications if current_user
   end
 
-  # TODO handle case: user logged in, auth known (different user)
   def create
     auth = request.env["omniauth.auth"]
-    auth['uid'] = auth['code'] if auth['provider'] == 'github'
-    authentication = Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
-    if authentication
-      puts 'auth known, user not logged in'                             # auth known, user not logged in
+    authentication = find_existing auth
+
+    if authentication && current_user.nil?                              # auth known, user not logged in
       flash[:notice] = t("devise.omniauth_callbacks.success", :kind => auth['provider'])
       sign_in_and_redirect(:user, authentication.user)
-    elsif current_user
-      puts 'auth not known, user logged in'                             # auth not known, user logged in
+    
+    elsif authentication.nil? && current_user                           # auth not known, user logged in
       current_user.apply_omniauth(auth)
       current_user.save
       redirect_to authentications_path, :notice => t('.authentication_successful')
-    else
-      puts 'auth not known, user not logged in'                         # auth not known, user not logged in
+    
+    elsif authentication.nil? && current_user.nil?                     # auth not known, user not logged in
       user = User.new
       user.apply_omniauth(auth)
-      if user.save
-        puts 'new user'
+    
+      if user.save                                                                # create new user with auth
         flash[:notice] = t('.sign_in_successful')
         sign_in_and_redirect(:user, user)
-      else
-        puts 'email missing'
+      else                                                                        # do not create user without email
         flash[:notice] = t('.email_needed', :provider => auth['provider'])
         session[:omniauth] = auth.except('extra')
         redirect_to new_user_registration_url
       end
+
+    else                                                              # auth known, different user logged in
+      redirect_to authentications_path, :alert => t('.authentication_bound_to_other_user')
     end
   end
 
@@ -38,6 +38,11 @@ class AuthenticationsController < ApplicationController
     @authentication = current_user.authentications.find(params[:id])
     @authentication.destroy
     redirect_to authentications_url, :notice => t('.destroy_authentication_success')
+  end
+
+  def find_existing(auth)
+    auth['uid'] = auth['code'] if auth['provider'] == 'github'              # github, or oauth 2.0 issue ??
+    Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
   end
 
   # to stop devise from destroying all sessions, when trying to add OpenId and this appears:
