a4a8635883c1ad5bbcdc3e1ed5e1508f07e8c6d7
Andreas Finger <webmaster@mediafinger.com>
2012-03-07 20:30:05 +0100

    Add GitHub oath 2.0 strategy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter,  ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :open_id,  :store => OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
-  provider :github,   ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']         #, :scope => "repo"
+  provider :github,   ENV['GITHUB_CLIENT_ID_EDGE'], ENV['GITHUB_SECRET_EDGE']         #, :scope => "repo"
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :open_id,  :store => OpenID::Store::Filesystem.new('./tmp')
   provider :xing,     ENV['XING_API_TEST_KEY_EDGE'], ENV['XING_API_TEST_SECRET_EDGE']
