c33115509e8a9380126804f011703ce2444228c1
Andreas Finger <webmaster@mediafinger.com>
2012-03-08 02:49:17 +0100

    Pimped startpage!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,24 @@
 module ApplicationHelper
 
-  def link_to_twitter(user, provider='twitter')
-    if user.nickname
-      link_to "@#{user.nickname}", "https://twitter.com/#!/#{user.nickname}"
+  def link_to_twitter(user)
+    if user.profile.nickname
+      link_to "@#{user.profile.nickname}", "https://twitter.com/#!/#{user.profile.nickname}"
+    else
+      ""
+    end
+  end
+
+  def link_to_website(user)
+    if user.profile.website
+      link_to "--> #{user.profile.website}", user.profile.website
+    else
+      ""
+    end
+  end
+
+  def profile_img(user)
+    if user.profile.thumbnail_url
+      image_tag(user.profile.thumbnail_url, :size => "48x48")
     else
       ""
     end
