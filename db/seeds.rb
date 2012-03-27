e36b2102730fb72b1c8fa20fa86baeb265a33458
Andreas Finger <webmaster@mediafinger.com>
2012-03-28 00:22:58 +0200

    Add User Fabricator and Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = [Fabricate(:andy), Fabricate(:andreas),
+        Fabricate(:user), Fabricate(:user), Fabricate(:user)]
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,11 +1,23 @@
 Fabricator(:user) do
-  nickname                { Faker::Name.first_name }
+  name                    { Faker::Name.first_name }
+  nickname                { '@' + Faker::Name.first_name + rand(99).to_s }
   email                   { |user| "#{user.nickname}@example.com".downcase }
   password                { |user| 'password' }
   password_confirmation   { |user| user.password }
 end
 
-Fabricator(:admin, :from => :user) do
-  nickname    'admin'
-  email       'admin@example.com'
+Fabricator(:andy) do
+  name                    { 'Andy' }
+  nickname                { '@hhandy' }
+  email                   { 'hhandy@gmx.de' }
+  password                { 'foobar' }
+  password_confirmation   { 'foobar' }
+end
+
+Fabricator(:andreas) do
+  name                    { 'Andreas Finger' }
+  nickname                { '@mediafinger' }
+  email                   { 'andreas.finger@tolingo.de' }
+  password                { 'foobar' }
+  password_confirmation   { 'foobar' }
 end
