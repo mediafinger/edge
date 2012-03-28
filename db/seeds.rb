f7db9dd8c475270cd59f3641696bde47cdde81d7
Andreas Finger <webmaster@mediafinger.com>
2012-03-28 18:53:56 +0200

    Test if different order will fix heroku / fabricator bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = [Fabricate(:andy), Fabricate(:andreas), Fabricate(:user), Fabricate(:user), Fabricate(:user)]
+users = [Fabricate(:user), Fabricate(:andy), Fabricate(:andreas), Fabricate(:user), Fabricate(:user)]
