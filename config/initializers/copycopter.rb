3a7159e4882c112901bac1d99391500986cafd35
Andreas Finger <webmaster@mediafinger.com>
2012-04-21 15:57:02 +0200

    Rename copycopter app to edge-words

--- config/initializers/copycopter.rb
+++ config/initializers/copycopter.rb
@@ -1,7 +1,7 @@
 CopycopterClient.configure do |config|
   # if ENV["RAILS_ENV"] == :production
     config.api_key = '404b4323eaf98dc39668a5a478ec6b51'
-    config.host = 'evening-samurai-2644.herokuapp.com'
+    config.host = 'edge-words.herokuapp.com'
   # else
   #   config.api_key = '1f3ec1897e14625f7b1f80c355890d55'
   #   config.host = 'localhost:5000'
