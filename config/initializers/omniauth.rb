e25ceba346899a023eaf14434e1e76992f2922a9
Andreas Finger <webmaster@mediafinger.com>
2012-11-11 22:03:22 +0100

    Add Procfile for dev env with Pow

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :open_id,  :store => OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
   provider :github,   ENV['GITHUB_CLIENT_ID_EDGE'], ENV['GITHUB_SECRET_EDGE']         #, :scope => "repo"
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :open_id,  :store => OpenID::Store::Filesystem.new('./tmp')
+#  provider :open_id,  :store => OpenID::Store::Filesystem.new('./tmp')
   provider :xing,     ENV['XING_API_TEST_KEY_EDGE'], ENV['XING_API_TEST_SECRET_EDGE']
 end
 
